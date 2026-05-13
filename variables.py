9b04abfce4563f63b7620086cd68834a35e18d7f
Hayden M <haydenmorocco@email.com>
2026-05-13 15:48:30 -0400

    Add Day 1 hello.py and Day 2 variables practice

---
+++ variables.py
@@ -0,0 +1,13 @@
+first_name = "Hayden"
+last_name = "Morocco"
+age = 19
+gpa = 4.0
+is_student = True
+print("Name:" , first_name , last_name)
+print("Age:" , age)
+print("GPA:" , gpa)
+print("Currently a student:" , is_student)
+
+print("Full name uppercase: ", first_name.upper(), last_name.upper())
+print("Name lenght:", len(first_name) + len(last_name))
+print("Greetings my name is", first_name, last_name, "and I am", age, "years old")
